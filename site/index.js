ee31ebeaa79090ddbc5bfbb8af32ad360ee80be9
Mark Wiemer <markwiemer@outlook.com>
2025-12-26 22:00:04 -0800

    chore: generate 1-20 using JS loop, not repeat code

--- site/index.js
+++ site/index.js
@@ -2,4 +2,15 @@ import * as dice from "./dice.js";
 
 console.log("Hello world");
 
-dice.printDiceMessage();
+const targetNumberSpan = document.getElementById('target-number');
+
+for (let i = 1; i <= 20; i++) {
+    const label = document.createElement('label');
+    const input = document.createElement('input');
+    input.type = 'radio';
+    input.name = 'target-number';
+    input.required = true;
+    label.appendChild(input);
+    label.appendChild(document.createTextNode(i));
+    targetNumberSpan.appendChild(label);
+}
