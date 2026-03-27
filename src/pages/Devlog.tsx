65be19372ddf055db09ee20d60a1829671a05095
mowiemer <mowiemer@outlook.com>
2026-03-26 22:42:44 -0400

    feat: reformat devlog page and clean up underlying folder structure

--- src/pages/Devlog.tsx
+++ src/pages/Devlog.tsx
@@ -1,14 +1,37 @@
 import MarkdownPage from "../components/MarkdownPage";
-import devlog from "../content/devlog.md?raw";
+import Collapsible from "../components/Collapsible";
 import { Link } from "react-router-dom"
 
-export default function DevLog() {
+
+const devlogs = import.meta.glob("../content/devlogs/*.md", {
+    query: "?raw",
+    import: "default",
+    eager: true,
+});
+
+
+export default function Devlog() {
     return (
-        <>
-          <span><Link to="/">
+        <div className = "page"> 
+
+            <span><Link to="/">
                 <button>Return to Home</button>
-              </Link></span> 
-            <MarkdownPage content={devlog} />
-        </>
-    )
+            </Link></span>
+
+            <h1>DevLog</h1>
+
+            {Object.entries(devlogs).map(([path, content]) => {
+                const filename = path.split("/").pop()!;
+                const name = filename.replace(".md", "").replaceAll("-", ".");
+                return (
+                    <div className="wrapper">
+                        <Collapsible key={path} label={name}>
+                            <MarkdownPage content={content as string} />
+                        </Collapsible>
+                    </div>
+                );
+            })}
+
+        </div>
+    );
 }
